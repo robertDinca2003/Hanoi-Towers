6bce8391a804ab6874e554b4d352aceb671258a9
Robert Dinca <71851178+robertDinca2003@users.noreply.github.com>
2022-10-08 12:30:46 +0300

    Add files via upload

---
+++ Hanoi Tower/Hanoi Tower/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Hanoi_Tower
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1058, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "End Game";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(279, 85);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(77, 26);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(191, 541);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 70);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(565, 541);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 70);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(932, 541);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 70);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "3";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(175, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Choose tower\'s scale (between 3 and 7)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Yellow;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Aqua;
+            this.label2.Location = new System.Drawing.Point(463, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 59);
+            this.label2.TabIndex = 8;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(842, 50);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(152, 44);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Auto Play";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1258, 664);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(1280, 720);
+            this.MinimumSize = new System.Drawing.Size(1280, 720);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hanoi Tower";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Hanoi Tower/Hanoi Tower/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Hanoi_Tower
+{
+    public partial class Form1 : Form
+    {
+
+        Dictionary<int, Color> pieceColor = new Dictionary<int, Color>();
+
+        bool GameStarted = false;
+
+        int[,] towers = new int[8, 4];
+
+        int[] t = { 0, 7, 1, 1 };
+
+        int nrMoves = 0;
+        int moves = 0;
+        int first, second;
+
+        //Start Game
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (GameStarted)
+            {
+                MessageBox.Show("You have to complete the game or press END GAME !");
+            }
+            else
+            {
+
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+                button6.Enabled = true;
+
+                label2.Text = "";
+                label1.BackColor = Color.White;
+
+                int maxSize = Convert.ToInt32(numericUpDown1.Value);
+
+                if (maxSize < 3 || maxSize > 7) {
+                    MessageBox.Show("Tower size out of range ( 3 - 7 )");
+                    return;
+                }
+
+                GameStarted = true;
+                numericUpDown1.Enabled = false;
+
+                t[1] = maxSize;
+                t[2] = 0;
+                t[3] = 0;
+
+                for (int i = maxSize; i >= 1; i--) { towers[i, 1] = maxSize - i + 1; towers[i, 2] = 0; towers[i, 3] = 0; }
+
+                Form1_Load(sender,e); 
+            }
+        }
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            pieceColor.Add(7, ColorTranslator.FromHtml("#336600"));
+            pieceColor.Add(6, ColorTranslator.FromHtml("#669900"));
+            pieceColor.Add(5, ColorTranslator.FromHtml("#cc0000"));
+            pieceColor.Add(4, ColorTranslator.FromHtml("#e600e6"));
+            pieceColor.Add(3, ColorTranslator.FromHtml("#cc33ff"));
+            pieceColor.Add(2, ColorTranslator.FromHtml("#3377ff"));
+            pieceColor.Add(1, ColorTranslator.FromHtml("#4ddbff"));
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button3.BackColor = Color.White;
+            button4.BackColor = Color.White;
+            button5.BackColor = Color.White;
+
+            Graphics g = this.CreateGraphics();
+            SolidBrush myBrush = new SolidBrush(Color.Red);
+            SolidBrush secondBrush = new SolidBrush(Color.Brown);
+
+            if (checkWinner())
+            {
+                g.Clear(Color.Yellow);
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+
+                label1.BackColor = Color.Yellow;
+                label2.Text = "YOU WON!";
+            }
+            else
+            g.Clear(Color.White);
+
+            int maxSize = Convert.ToInt32(numericUpDown1.Value);
+
+            int x = 150 , y = 120 + (7-maxSize) * 30, h = 20 , w = 30;
+
+
+            if(GameStarted == true)
+            {
+                g.FillRectangle(secondBrush, new Rectangle(x+10, y, 10, maxSize*30+10));
+                g.FillRectangle(secondBrush, new Rectangle(x-70, y+maxSize*30-10, 170, 20));
+
+                g.FillRectangle(secondBrush, new Rectangle(x + 255, y, 10, maxSize * 30 + 10));
+                g.FillRectangle(secondBrush, new Rectangle(x + 175, y + maxSize * 30 - 10, 170, 20));
+
+                g.FillRectangle(secondBrush, new Rectangle(x + 500, y, 10, maxSize * 30 + 10));
+                g.FillRectangle(secondBrush, new Rectangle(x + 420, y + maxSize * 30 - 10, 170, 20));
+
+            }
+            
+
+
+            for (int i = maxSize; i >= 1; i--)
+            {
+                if(towers[i,1] != 0)
+                {
+                    g.FillRectangle(new SolidBrush(pieceColor[towers[i,1]]), new Rectangle(x-(towers[i,1]-1)*10,y,w+(towers[i,1]-1)*20,h));
+                    
+                }
+
+                if (towers[i, 2] != 0)
+                {
+                    g.FillRectangle(new SolidBrush(pieceColor[towers[i, 2]]), new Rectangle(x+245 - (towers[i,2] - 1) * 10, y, w + (towers[i,2] - 1) * 20, h));
+                    
+                }
+                if (towers[i, 3] != 0)
+                {
+                    g.FillRectangle(new SolidBrush(pieceColor[towers[i, 3]]), new Rectangle(x+490 - (towers[i,3] - 1) * 10, y, w + (towers[i,3] - 1) * 20, h));
+                    
+                }
+                y += 30;
+            }
+
+            
+
+            g.Dispose();
+            myBrush.Dispose();
+        }
+
+        // End Game
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            GameStarted = false;
+            numericUpDown1.Value = 0;
+            numericUpDown1.Enabled = true;
+
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = true;
+
+            label1.BackColor = Color.White;
+            label2.Text = "";
+
+            for (int i = 7; i >= 1; i--) { towers[i, 1] = 0; towers[i, 2] = 0; towers[i, 3] = 0; }
+
+            Form1_Load(sender, e);
+        }
+
+        //1
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.BackColor = Color.Gray;
+            moves++;
+            if(moves%2 == 1)
+            {
+                first = 1;
+            }
+            else
+            {
+                second = 1;
+                makeMove(first,second,sender,e);
+            }
+        }
+
+        //2
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button4.BackColor = Color.Gray;
+            moves++;
+            if (moves % 2 == 1)
+            {
+                first = 2;
+            }
+            else
+            {
+                second = 2;
+                makeMove(first, second, sender, e);
+            }
+        }
+
+        //3
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button5.BackColor = Color.Gray;
+            moves++;
+            if (moves % 2 == 1)
+            {
+                first = 3;
+            }
+            else
+            {
+                second = 3;
+                makeMove(first, second, sender, e);
+            }
+        }
+
+        private bool checkWinner()
+        {
+            if (GameStarted == false) return false;
+            int maxSize = Convert.ToInt32(numericUpDown1.Value);
+
+            int ok = 1; 
+            for (int i = 1; i <= maxSize; i++) if (towers[i, 2] == 0) ok = 0;
+            if (ok == 1) return true;
+            ok = 1;
+            for (int i = 1; i <= maxSize; i++) if (towers[i, 3] == 0) ok = 0;
+            if (ok == 1) return true;
+            return false;
+        }
+
+        private bool validMove(int from, int to)
+        {
+            //MessageBox.Show($"{towers[t[from], from]} --- {towers[t[to], to]}" );
+            if (from == to) return false;
+            if (towers[t[from], from] == 0) return false;
+            if (towers[t[to], to] == 0) return true;
+            if (towers[t[from], from] < towers[t[to], to]) return true;
+            else return false;
+        }
+
+        
+
+        private void makeMove(int from, int to, object sender, EventArgs e)
+        {
+            button6.Enabled = false;
+            if (!validMove(from, to)) { MessageBox.Show("Invalid Move!"); Form1_Load(sender,e); return; }
+            towers[t[to]+1, to] = towers[t[from], from];
+            t[to]++;
+            towers[t[from], from] = 0;
+            t[from]--; 
+            Form1_Load(sender, e);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (!GameStarted)
+            {
+                MessageBox.Show("Game is not started!");
+                return; 
+            }
+            int maxSize = Convert.ToInt32(numericUpDown1.Value);
+
+
+            if(maxSize % 2 == 1)
+            {
+                for(int i = 1; i < Math.Pow(2,maxSize); i++)
+                {
+                    System.Threading.Thread.Sleep(500);
+                    if (i % 3 == 1)
+                    {
+                        if (validMove(1,3)) makeMove(1,3,sender,e);
+                        else makeMove(3,1,sender,e);
+                    }
+                    if(i % 3 == 2)
+                    {
+                        if (validMove(1,2)) makeMove(1,2,sender,e);
+                        else makeMove(2,1,sender,e);
+                    }
+                    if (i % 3 == 0)
+                    {
+                        if (validMove(2,3)) makeMove(2,3,sender,e);
+                        else makeMove(3,2,sender,e);
+                    }
+
+                }
+
+            }
+            else
+            {
+                for(int i = 1; i < Math.Pow(2, maxSize); i++)
+                {
+                    System.Threading.Thread.Sleep(500);
+                    if (i % 3 == 1)
+                    {
+                        if (validMove(1, 2)) makeMove(1, 2, sender, e);
+                        else makeMove(2, 1, sender, e);
+                    }
+                    if (i % 3 == 2)
+                    {
+                        if (validMove(1, 3)) makeMove(1, 3, sender, e);
+                        else makeMove(3, 1, sender, e);
+                    }
+                    if (i % 3 == 0)
+                    {
+                        if (validMove(2, 3)) makeMove(2, 3, sender, e);
+                        else makeMove(3, 2, sender, e);
+                    }
+
+                }
+            }
+
+        }
+    }
+}
